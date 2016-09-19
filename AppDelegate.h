ca1887d79465379f15220cf37057f499d311a42c
ruzzgames <rob@ruzzgames.com>
2016-09-19 18:13:32 +1000

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlindFate
+//
+//  Created by Robert Ruzbacky on 18/09/2016.
+//  Copyright © 2016 Robert Ruzbacky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
