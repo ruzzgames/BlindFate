59f04d29a648c28c7db9b7ebde6b520b1cddcfdc
ruzzgames <rob@ruzzgames.com>
2020-05-09 13:52:14 +1000

    Add files via upload

---
+++ mac/Blindfate MacOS/AppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.m
+//  Blindfate MacOS
+//
+//  Created by Robert Ruzbacky on 26/4/20.
+//  Copyright © 2020 RuzzGames. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+@end
